5d36e285740f7060151b774462071b5c58bc4dfb
Carlos Enrique <carlos.figueroa36422@outlook.com>
2023-04-26 03:14:23 -0600

    FIX: Probably delete

--- src/views/gato.vue
+++ src/views/gato.vue
@@ -1,26 +1,76 @@
 <script setup>
     import {ref }from 'vue'
     import Celda   from './celda.vue';
-    const tablero = []
-    const fila= [0,0,0]
+    // const tablero = []
+    // const fila= ['','','']
 
-    for(let i=0; i<3; i++){
-        tablero.push(fila)
-    }
+    // for(let i=0; i<3; i++){
+    //     tablero.push(fila)
+    // }
 
-    const turno = ref(1)
+    // const turno = ref(1)
 
-    turno.value = 1
+    // turno.value = 1
     
-    function movimiento(x, y){
-        tablero[x][y] = turno.value
+    // function movimiento(x, y){
+    //     tablero[x][y] = turno.value
 
-        if(turno.value == 1){
-            turno.value=2
+    //     if(turno.value == 1){
+    //         turno.value=2
+    //     }
+    //     else if (turno.value == 2){
+    //         turno.value = 1
+    // }
+    // }
+
+    //-----------------------------
+    
+    import Players from './home.vue'
+
+	const tablero = ref([
+      ["","",""],
+      ["","",""],
+      ["","",""]
+    ])
+
+	const winner =(tablero) =>{
+		const keys = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ]
+	  for (var i = 0; i < keys.length; i++) {
+      const [a, b, c] = keys[i]
+        if (tablero[a] && tablero[a] === tablero[b] && tablero[a] === tablero[c]) {
+          return tablero[a]
         }
-        else if (turno.value == 2){
-            turno.value = 1
-    }
+      }
+      return null
+	}
+
+	const jugador=ref("x")
+
+	const turno = (x,y) => {
+      if (tablero.value[x][y] !== "") return;
+
+      tablero.value [x][y] = jugador.value;
+
+      if(winner(tablero.value.flat())){
+        isGameOver.value = true;
+      }else if(tablero.value.flat().every((val)=>val!= "")){
+        isGameOver.value = true;
+      }
+      if (jugador.value==='o'){
+        jugador.value='x'
+      }
+      else{
+        jugador.value='o'
+      }
     }
 
 </script>
@@ -30,8 +80,8 @@
         <template v-for="(_, i) in tablero">
 
         <div v-for="(_, j) in tablero[i]">
-            <div>
-                <Celda @click="$emit(movimiento(i,j))" :volteado="tablero[i][j]" />
+            <div class="hover cursor-pointer" @click="turno(i,j)">
+                <Celda  :volteado="tablero[i][j]"/>
             </div>
         </div>
 
