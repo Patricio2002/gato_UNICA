f9bc08deb2204b559969b8db7194b400e34a970f
Carlos Enrique <carlos.figueroa36422@outlook.com>
2023-04-25 21:28:33 -0600

    router adder

---
+++ src/router/index.router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/:catchAll(.*)",
+            redirect: { name: "Home" },
+        },
+        {
+            path: '/',
+            name: 'Home',
+            component: () => import('../views/home.vue'),
+        },
+        {
+            path: '/tablero',
+            name: 'tablero',
+            component: () => import('../views/tablero.vue')
+        }
+    ]
+})
+
+export default router
